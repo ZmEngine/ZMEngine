e259b96461d239cc102b9de9b23886cfdc36d9da
ZhangMeng <496472571@qq.com>
2018-07-31 16:37:36 +0800

    补充缺失文件

--- server/config.js
+++ server/config.js
@@ -1,6 +1,14 @@
 const CONF = {
     port: '5758',
     rootPathname: '',
+    mysql: {
+        host: '',
+        port: 3306,
+        user: '',
+        db: '',
+        pass: '',//'xJScl8aA',
+        char: ''
+    }
 }
 
 module.exports = CONF
--- server/routes/auto-routes.js
+++ server/routes/auto-routes.js
@@ -3,17 +3,6 @@ const router = require('koa-router')({
   prefix: '/weapp'
 });
 const controllers = require('../controllers')
-// 从 sdk 中取出中间件
-// 这里展示如何使用 Koa 中间件完成登录态的颁发与验证
-const { auth: { authorizationMiddleware, validationMiddleware } } = require('../qcloud')
-
-// --- 登录与授权 Demo --- //
-// 登录接口
-router.get('/login', authorizationMiddleware, controllers.login)
-// 用户信息接口（可以用来验证登录态）validationMiddleware, validationMiddleware,
-router.get('/user',  controllers.user)
-//上传接口
-router.post('/nouserupload/:action', controllers.upload)
 function addMapping(router, mapping,url) {
   //console.log(`_x___${mapping}_/${url}__z_`);
   let ctr = controllers[url];
